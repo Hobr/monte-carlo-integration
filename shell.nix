f75518e8badd4c04ed6d5e3b1127b6ed746c7cda
Hobr <mail@hobr.site>
2024-04-01 07:38:43 +0800

    cuda

--- shell.nix
+++ shell.nix
@@ -6,7 +6,35 @@
   ...
 }:
 pkgs.mkShell {
-  packages = with pkgs; [
+  buildInputs = with pkgs; [
+    git
+    gitRepo
+    gnupg
+    autoconf
+    curl
+    procps
+    gnumake
+    util-linux
+    m4
+    gperf
+    unzip
+    libGLU
+    libGL
+    xorg.libXi
+    xorg.libXmu
+    freeglut
+    xorg.libXext
+    xorg.libX11
+    xorg.libXv
+    xorg.libXrandr
+    zlib
+    ncurses5
+    stdenv.cc
+    binutils
+    cudatoolkit
+    cudaPackages.cudnn
+    linuxPackages.nvidia_x11
+    julia
     (python3.withPackages (python-pkgs:
       with python-pkgs; [
         isort
@@ -18,6 +46,11 @@ pkgs.mkShell {
         #cupy
         scipy
       ]))
-    julia
   ];
+  shellHook = ''
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
+    export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+    export EXTRA_CCFLAGS="-I/usr/include"
+  '';
 }
