ad1e3c5389ab3b9ec14cb2f9fef2be34f07d0113
Hobr <mail@hobr.site>
2024-03-31 13:44:29 +0800

    cuda?

--- shell.nix
+++ shell.nix
@@ -1,16 +1,23 @@
-{pkgs ? (import <nixpkgs> {config.allowUnfree = true;}), ...}:
+{
+  pkgs ? (import <nixpkgs> {
+    config.allowUnfree = true;
+    cudaSupport = true;
+  }),
+  ...
+}:
 pkgs.mkShell {
   packages = [
     (pkgs.python3.withPackages (python-pkgs: [
-      python-pkgs.pandas
-      python-pkgs.numpy
-      python-pkgs.numba
-      python-pkgs.matplotlib
       python-pkgs.isort
       python-pkgs.black
       python-pkgs.flake8
       python-pkgs.pip
+      python-pkgs.numpy
+      python-pkgs.numba
+      python-pkgs.matplotlib
     ]))
     pkgs.julia
+    pkgs.cudatoolkit
+    #pkgs.linuxPackages.nvidia_x11
   ];
 }
