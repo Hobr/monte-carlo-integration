6fcac15b4fe67561156c9d2ba6489b6d8f6cc93c
Hobr <mail@hobr.site>
2024-03-31 21:23:40 +0800

    format

--- shell.nix
+++ shell.nix
@@ -7,16 +7,17 @@
 }:
 pkgs.mkShell {
   packages = with pkgs; [
-    (python3.withPackages (python-pkgs: with python-pkgs; [
-      isort
-      black
-      flake8
-      pip
-      numbaWithCuda
-      matplotlib
-      cupy
-      scipy
-    ]))
+    (python3.withPackages (python-pkgs:
+      with python-pkgs; [
+        isort
+        black
+        flake8
+        pip
+        numbaWithCuda
+        matplotlib
+        cupy
+        scipy
+      ]))
     julia
   ];
 }
